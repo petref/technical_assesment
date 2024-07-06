01da0640ea21aebfcc330c5062bf1914fa806405
petref <petroupb@gmail.com>
2024-07-06 14:45:18 +0300

    Add express server

--- server/api/express.js
+++ server/api/express.js
@@ -3,14 +3,15 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import https from "https";
 import fs from "fs";
+import 'dotenv/config'
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
 const server = https.createServer({
-    cert: fs.readFileSync("./certs/sssl/cert.pem"),
-    key: fs.readFileSync('./certs/sssl/key.pem'),
+    cert: fs.readFileSync("./certs/ssl/cert.pem"),
+    key: fs.readFileSync('./certs/ssl/key.pem'),
     passphrase: "Greutrece@33"
 }, app);
 
