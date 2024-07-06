5f829dab7cc3b900126e3dc2102db1c9ad7e744d
petref <petroupb@gmail.com>
2024-07-07 01:23:53 +0300

    Add socket to entry point

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
-import server from "./api/express.js";
+import server from "./services/express.js";
 
+import webSocket from "./services/websocket.js";
+
+
+webSocket(server);
 
 server.listen(process.env.PORT, () => {console.log(`Listening on ${process.env.PORT}`)})
