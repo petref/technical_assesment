718bff8242735367a5ccc6005be4a9c05b436de0
petref <petroupb@gmail.com>
2024-07-07 23:43:03 +0300

    Add initiate db to entrypoint

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import server from "./services/express.js";
 
 import webSocket from "./services/websocket.js";
-
+import db from "./database/index.js";
 
 webSocket(server);
+db.connectDB();
 
 server.listen(process.env.PORT, () => {console.log(`Listening on ${process.env.PORT}`)})
