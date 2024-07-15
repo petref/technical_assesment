795458dc0d638f6b8146653a84e6ef04da3e617f
petref <petroupb@gmail.com>
2024-07-16 01:44:08 +0300

    Add logout controller method and mongodb functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,17 +1,43 @@
 import { generateToken } from "../services/jwt.js";
+import getDB from "../database/index.js";
 
-const auth = (req, res) => {
+const auth = async (req, res) => {
     const { username, password } = req.body;
     // Validate user credentials (in a real application, you should check against a database)
-    if (username === 'user' && password === 'password') {
+    const db = getDB.getDB("users");
+    const users = db.collection("users");
+   
+
+    const user = await users.find({$and: [{userName: username},{password}]}).toArray();
+
+    if(!user?.[0]) return res.sendStatus(403);
+
+    if (user[0]?.userName === username && user[0]?.password === password ) {
         const user = { username };
-        const token = generateToken(user);
+        const token = generateToken({username});
+        await users.findOneAndUpdate({userName: username}, {$set: {isLoggedIn: true}})
         res.json({ token });
     } else {
         res.sendStatus(403);
     }
 }
 
+const logOut = async (req, res) => {
+    const { username } = req.body;
+
+    const db = getDB.getDB("users");
+    const users = db.collection("users");
+    const user = await users.find({userName: username}).toArray();
+
+    if (user) {
+        await users.findOneAndUpdate({userName: username}, {$set: {isLoggedIn: false}})
+    };
+
+    res.sendStatus(200)
+
+}
+
 export {
     auth,
+    logOut,
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {auth} from "../controllers/auth.js";
+import {auth, logOut} from "../controllers/auth.js";
 import { authenticateToken } from "../services/jwt.js"
 
 import { publicKeyPem } from "../certs/scripts/utils.js";
@@ -8,6 +8,8 @@ import { publicKeyPem } from "../certs/scripts/utils.js";
 const router = express.Router();
 
 router.post('/login', auth);
+router.post('/logout', logOut);
+
 
 router.get('/public-key', authenticateToken, (req, res) => {
     res.json({ publicKey: publicKeyPem });
