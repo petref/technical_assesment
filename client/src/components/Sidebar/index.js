4fbc58cfeb5120d04d4bac63082d422de881c53a
petref <petroupb@gmail.com>
2024-07-18 14:08:37 +0300

    Add navigation to chatpage

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,17 +1,16 @@
 import { useState } from 'react';
 import { styled } from '@mui/system';
 import Box from '@mui/material/Box';
+import { Link } from "react-router-dom";
+import PowerIcon from '@mui/icons-material/Power';
+import PowerOffIcon from '@mui/icons-material/PowerOff';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import { useWebSocket } from '../../context/WithWebSocket';
+import { useTheme } from '@mui/material/styles';
 
 
 
@@ -30,6 +29,7 @@ const SwipeableTemporaryDrawer = ({ setSideBar, isDrawerOpen }) => {
 
     const [selectedList, setSelectedList] = useState('list1');
     const { rooms, users } = useWebSocket();
+    const theme = useTheme();
 
     const handleListChange = (list) => {
         setSelectedList(list);
@@ -47,41 +47,6 @@ const SwipeableTemporaryDrawer = ({ setSideBar, isDrawerOpen }) => {
         // setState({ ...state, [anchor]: open });
         setSideBar()
     };
-
-    const list = (anchor) => (
-        <Box
-            sx={{ width: 250 }}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
     return (
         <div>
             <>
@@ -105,18 +70,23 @@ const SwipeableTemporaryDrawer = ({ setSideBar, isDrawerOpen }) => {
                             Utilizatori
                         </TabButton>
                     </ButtonContainer>
-                        {(rooms && users) && (selectedList === 'list1'
-                            ? rooms.map((room) => (
-                                <ListItem button key={room._id}>
-                                    <ListItemText primary={room.roomName} />
-                                </ListItem>
-                            ))
-                            : users.map((item, index) => (
-                                <ListItem button key={item._id}>
-                                    <ListItemText primary={item.name} />
-                                </ListItem>
-                            ))
-                        )}
+                    {(rooms && users) && (selectedList === 'list1'
+                        ? rooms.map((room) => (
+                            <Link to={`/room?roomName=${room?.roomName}`} style={{"textDecoration": "unset", color: theme.palette.primary.main}}>
+                                <ListItemButton key={room?._id}>
+                                    <ListItemText primary={room?.roomName} />
+                                </ListItemButton >
+                            </Link>
+                        ))
+                        : users.map((item, index) => (
+                            <ListItemButton key={item?._id} >
+                                <ListItemIcon>
+                                    {item?.isLoggedIn ? <PowerIcon color={theme.palette.primary.dark} /> : <PowerOffIcon color='secondary.main' />}
+                                </ListItemIcon>
+                                <ListItemText primary={item?.userName} />
+                            </ListItemButton >
+                        ))
+                    )}
                     {/* {list("left")} */}
                 </SwipeableDrawer>
             </>
